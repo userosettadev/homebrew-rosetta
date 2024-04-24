6c08d2a735a1bcccecfd288568135aaa5d66738b
Effi <effi@userosetta.com>
2024-04-24 11:34:16 +0300

    Brew formula update for rosetta-cli version v0.0.17

--- rosetta.rb
+++ rosetta.rb
@@ -5,11 +5,11 @@
 class Rosetta < Formula
   desc ""
   homepage "https://github.com/userosettadev/rosetta-cli"
-  version "0.0.16"
+  version "0.0.17"
 
   on_macos do
-    url "https://github.com/userosettadev/rosetta-cli/releases/download/v0.0.16/rosetta-cli_0.0.16_darwin_all.tar.gz"
-    sha256 "f2c2d78e99405e14976ad78bf12f37eaf06a0d03a595d7d6814be5b608539ab8"
+    url "https://github.com/userosettadev/rosetta-cli/releases/download/v0.0.17/rosetta-cli_0.0.17_darwin_all.tar.gz"
+    sha256 "fe7a7ed2cc4d9aa3fae32ba05ec7628105af968459de2ec5cdfaa48bf120bf8d"
 
     def install
       bin.install "rosetta"
@@ -18,16 +18,16 @@ class Rosetta < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/userosettadev/rosetta-cli/releases/download/v0.0.16/rosetta-cli_0.0.16_linux_amd64.tar.gz"
-      sha256 "534f78c94364903aa1d56625cf6eeead52a720d3b982ad374873188a5125ac26"
+      url "https://github.com/userosettadev/rosetta-cli/releases/download/v0.0.17/rosetta-cli_0.0.17_linux_amd64.tar.gz"
+      sha256 "9992063b0657dc98ec0b8a47288d6689abc9d609559ecbc3e6d75ac0e93b58ee"
 
       def install
         bin.install "rosetta"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/userosettadev/rosetta-cli/releases/download/v0.0.16/rosetta-cli_0.0.16_linux_arm64.tar.gz"
-      sha256 "51f9232e0b8db75e4cc3d82ff3c6e39346143c133cdd5ee786adc47a23e49853"
+      url "https://github.com/userosettadev/rosetta-cli/releases/download/v0.0.17/rosetta-cli_0.0.17_linux_arm64.tar.gz"
+      sha256 "e929ddae4baeb62fe5bf8e0db3c19bccf6e039472d6ca43cb79453caf8073787"
 
       def install
         bin.install "rosetta"
